1af4d7bdca9030123b68c5e64988b9e538b203ae
Harsh Zadafiya <harshzadafiya@harshs-air.home>
2023-10-22 11:38:12 -0400

    assignment 1 done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 // Harsh Zadafiya 8866930 - backend/fix bug
 // Rohit Banwal   8879715 - frontend
-// Kishan Joshi
+// Kishan Joshi   8816399 - graphql
+// Ramandeep kaur 8885823 - frontend
 
 import EmpList from "./components/EmpList";
 
